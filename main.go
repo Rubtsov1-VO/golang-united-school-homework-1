e6e94fc45494ea6abd1dead436170d721b26adb9
vladimirrubtsov <Vladimir08!>
2022-02-13 22:13:55 +0300

    new

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package solution
 
 import (
 	"github.com/kyokomi/emoji/v2"
-	"fmt"
+
 )
 
 func GetMessage() string {
 	hello := "Hello"
 	world := ":world_map:"
 	rendered := emoji.Sprint(hello, world)
-	rendered = rendered + " !"
-	return fmt.Println(rendered)
+	rendered = rendered + "!"
+	return rendered
 
 }
