effe2073573439c012b85eeb607f37dc660b96f6
vladimirrubtsov <Vladimir08!>
2022-02-13 09:46:44 +0300

    new

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-    "github.com/kyokomi/emoji/v2"
+	"github.com/kyokomi/emoji/v2"
 )
 
-func GetMessage() {
+func main() {
 	hello := emoji.Sprint("Hello, :world_map:!")
 	fmt.Println(hello)
 }
